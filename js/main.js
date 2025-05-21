5cf886f3b0bec57116da7d501e389e3e7fdc2a43
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2025-05-21 12:46:08 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('NAHH MUNGEEE').split('')
+    const titles = ('NAHH').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
